2f4f3e912fc9078bb156b02cbdd58fb24b8931ee
Your Name <you@example.com>
2024-02-07 23:57:49 +0800

    fixed bg

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,12 @@ import "./styles.css";
 import { DM_Sans } from "next/font/google";
 import { Tabs } from "./components/tabs";
 import "./tabs.css";
-import type { Metadata } from 'next'
- 
+import type { Metadata } from "next";
+
 export const metadata: Metadata = {
-  title: 'simple coffee page listing',
-  description: '...',
-}
+  title: "simple coffee page listing",
+  description: "...",
+};
 
 /*
 //
@@ -19,20 +19,22 @@ const dmsans = DM_Sans({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main id="main">
-      <div className="bigCon">
-        <header>
-          <h1 className={`${dmsans.className} ourCollection`}>
-            Our Collection
-          </h1>
-          <p className={`${dmsans.className} headerText`}>
-            Introducing our Coffee Collection, a selection of unique coffees
-            from different roast types and origins, expertly roasted in small
-            batches and shipped fresh weekly.
-          </p>
-        </header>
-        <Tabs />
+    <div className="biggest">
+      <div id="main">
+        <div className="bigCon">
+          <header>
+            <h1 className={`${dmsans.className} ourCollection`}>
+              Our Collection
+            </h1>
+            <p className={`${dmsans.className} headerText`}>
+              Introducing our Coffee Collection, a selection of unique coffees
+              from different roast types and origins, expertly roasted in small
+              batches and shipped fresh weekly.
+            </p>
+          </header>
+          <Tabs />
+        </div>
       </div>
-    </main>
+    </div>
   );
 }
